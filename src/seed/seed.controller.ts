22e1abea3c2459d1e68a2eff0c358aa1f893597e
hdavella <hdavella@gmail.com>
2025-03-15 12:12:49 -0300

    Seed clean methods

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -3,25 +3,11 @@ import { SeedService } from './seed.service';
 
 @Controller('seed')
 export class SeedController {
+
   constructor(private readonly seedService: SeedService) {}
 
-  @Post()
-  create() {
-    return
-  }
-
   @Get()
-  findAll() {
-    return this.seedService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.seedService.findOne(+id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.seedService.remove(+id);
+  executeSeed() {
+    return this.seedService.executeSeed();
   }
 }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,23 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SeedService {
-  create() {
-    return 'This action adds a new seed';
+
+  executeSeed() {
+    return 'This action adds seed';
   }
 
-  findAll() {
-    return `This action returns all seed`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} seed`;
-  }
-
-  update() {
-    return `This action updates a seed`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} seed`;
-  }
 }
