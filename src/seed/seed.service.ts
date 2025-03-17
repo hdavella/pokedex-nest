ba87bae338015e85059bdc8962a7a398a32907ff
hdavella <hdavella@gmail.com>
2025-03-17 13:42:08 -0300

    Axios funcionando

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
 
-  executeSeed() {
-    return 'This action adds seed';
-  }
+  private readonly axios: AxiosInstance= axios;
 
+  async executeSeed() {
+    console.log(fetch);
+    const {data} = await this.axios.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=10");
+    data.results.forEach( ({name, url})=> {
+      const segments= url.split('/');
+      const no = +segments[segments.length -2];
+    })
+    return data.results;
+  }
 }
