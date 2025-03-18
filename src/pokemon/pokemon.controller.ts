b8cd65867ee5180bddfa1cc8e27c2709878c6e1c
hdavella <hdavella@gmail.com>
2025-03-18 17:38:26 -0300

    httpAdapter

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
@@ -14,7 +14,8 @@ export class PokemonController {
   }
 
   @Get()
-  findAll() {
+  findAll(@Query() queryParameter) {
+    console.log(queryParameter);
     return this.pokemonService.findAll();
   }
 
